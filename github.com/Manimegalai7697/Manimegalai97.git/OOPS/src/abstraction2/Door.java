f0a2df236d65ab392fb937402c7af1555e12bdae
Manimegalai <Manimegalai@DESKTOP-0OFPGHK>
2020-01-04 16:48:24 +0530

    Pattern

---
+++ github.com/Manimegalai7697/Manimegalai97.git/OOPS/src/abstraction2/Door.java
@@ -0,0 +1,18 @@
+package abstraction2;
+
+public abstract class Door {
+		protected String doorName;
+		public Door(String doorName) {
+		super();
+		this.doorName = doorName;
+		}
+		public String getDoorName() {
+		return doorName;
+		}
+		public void setDoorName(String doorName) {
+		this.doorName = doorName;
+		}
+
+
+
+}
