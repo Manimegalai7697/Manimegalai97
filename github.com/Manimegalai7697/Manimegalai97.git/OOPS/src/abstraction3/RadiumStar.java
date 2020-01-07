1167269fabf0eecdf40666c0d5d04098dc3486e5
Manimegalai <Manimegalai@DESKTOP-0OFPGHK>
2020-01-07 12:18:01 +0530

    star

---
+++ github.com/Manimegalai7697/Manimegalai97.git/OOPS/src/abstraction3/RadiumStar.java
@@ -0,0 +1,17 @@
+package abstraction3;
+
+public class RadiumStar extends Star{
+	private int decayIndex,temp;    
+	public RadiumStar(String starName, String constellationname, int decayIndex, int energyToEmit, int age,int energyConsume) {
+		super(starName,constellationname,age,energyToEmit,energyConsume);
+		this.decayIndex=decayIndex;
+	}
+	int  calculateYearsRemaining()
+	{
+		temp=(decayIndex*(decayIndex+1))/2;
+		return temp;
+	}
+}
+
+
+
